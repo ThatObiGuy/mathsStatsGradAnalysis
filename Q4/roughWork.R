7bb07cffd97f40f22c17f27d4d9ada064b077ef3
Owen F. O'Connor <owen.oconnor.2024@mumail.ie>
2025-03-19 21:30:43 +0000

    Data Pre-processing + gitignore
    initial data processing in roughWork.R and added a gitignore to keep things tidy.

--- Q4/roughWork.R
+++ Q4/roughWork.R
@@ -1,1 +1,45 @@
 # Rough work - data exploration
+
+library(tidyverse)
+load("gradMS.Rdata")
+
+# Data cleaning / pre-processing 
+gradMS[,1] # statistic
+gradMS[,2] # year
+gradMS[,3] # gender
+gradMS[,4] # field of study
+gradMS[,5] # NFQ Level
+gradMS[,6] # years since graduation
+gradMS[,7] # measure of statistic
+
+# field of study and NFQ are irrelevant since they're always the same. We can remove them
+gradMS <- gradMS[,-c(4,5)]
+gradMS[,1] # statistic
+gradMS[,2] # year
+gradMS[,3] # gender
+gradMS[,4] # years since graduation
+gradMS[,5] # measure of statistic
+
+gradMS %>%
+  filter(Gender == "Other gender")
+# there only exists one observations
+# we can't make any meaningful analysis off of one point.
+# I am hence deciding to omit this data
+
+gradMS <- gradMS %>%
+  filter(Gender != "Other gender") # this removes 300 rows
+
+# I think it'd also make sense to pivot this table wider
+# Increases readability and will make ggplots easier
+
+gradMS_wider <- gradMS |> # I use a base pipe here because magrittr pipes play weird with '.'
+  pivot_wider(names_from = Statistic, values_from = .)
+
+# Looking at 'gradMS_wider', now a lot more of the NA's make sense
+# logically, if this data is from 2019, they can only have earnings of observations 1 year from gradudation,
+# and two years from graduation for those from 2018, and so on backwards. . .
+
+gradMS_wider_lighter <- na.omit(gradMS_wider)
+
+# very elegant solution to this ^
+# I think now we have our data in forms well suited for answering our questions.
